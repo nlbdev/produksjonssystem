ea5470606ff96246908aa054edf873799f0caf7c
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2017-10-26 13:58:33 +0200

    moved Pipeline class into core

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -10,7 +10,7 @@ import time
 from pathlib import Path
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from produksjonssystem.pipeline import Pipeline
+from produksjonssystem.core.pipeline import Pipeline
 
 class PipelineTest(unittest.TestCase):
     target = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'target')
