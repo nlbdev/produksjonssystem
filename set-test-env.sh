fa3457f1110a1f0bf7880d734e34126f9e9079c5
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2018-04-19 16:01:48 +0200

    set CONFIG_FILE test env

--- set-test-env.sh
+++ set-test-env.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+
+# script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 export BOOK_ARCHIVE_DIRS="master=/tmp/book-archive share=/tmp/book-archive-share"
 export TRIGGER_DIR="/tmp/trigger-produksjonssystem"
 export PIPELINE2_HOME="$HOME/Desktop/daisy-pipeline"
 export ORIGINAL_ISBN_CSV="$HOME/Desktop/original-isbn.csv"
+
+if [ -f "$HOME/Desktop/produksjonssystem.yaml" ]; then
+    export CONFIG_FILE="$HOME/Desktop/produksjonssystem.yaml"
+else
+    export CONFIG_FILE="$DIR/produksjonssystem.yaml"
+fi
