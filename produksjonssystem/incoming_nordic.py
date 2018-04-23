62e7aa33c4b2c60c727daa8c12d383c44c45d5f6
Espen Solhjem <espen.solhjem@nlb.no>
2018-04-23 14:45:40 +0200

    kwargs in incoming_nordic

--- produksjonssystem/incoming_nordic.py
+++ produksjonssystem/incoming_nordic.py
@@ -24,60 +24,60 @@ if sys.version_info[0] != 3 or sys.version_info[1] < 5:
 class IncomingNordic(Pipeline):
     uid = "incoming-nordic"
     title = "Validering av Nordisk EPUB 3"
-    
+
     ace_cli = None
-    
+
     @staticmethod
     def init_environment():
         IncomingNordic.ace_cli = Pipeline.environment["ACE_CLI"] if "ACE_CLI" in Pipeline.environment else "/usr/bin/ace"
-    
-    def __init__(self):
+
+    def __init__(self, *args, **kwargs):
         IncomingNordic.init_environment()
-        super().__init__()
-    
+        super().__init__(*args, **kwargs)
+
     def on_book_deleted(self):
         self.utils.report.should_email = False
-    
+
     def on_book_modified(self):
         self.utils.report.info("Endret bok i mappa: "+self.book['name'])
         self.on_book()
-    
+
     def on_book_created(self):
         self.utils.report.info("Ny bok i mappa: "+self.book['name'])
         self.on_book()
-    
+
     def on_book(self):
         epub = Epub(self, self.book["source"])
-        
+
         # sjekk at dette er en EPUB
         if not epub.isepub():
             self.utils.report.title = self.title + ": " + self.book["name"] + " feilet 😭👎"
             return
-        
+
         if not epub.identifier():
             self.utils.report.error(self.book["name"] + ": Klarte ikke å bestemme boknummer basert på dc:identifier.")
             self.utils.report.title = self.title + ": " + self.book["name"] + " feilet 😭👎"
             return
-        
+
         self.utils.report.info("Validerer EPUB...")
         dp2_job = DaisyPipelineJob(self, "nordic-epub3-validate", { "epub": epub.asFile() })
-        
+
         # get validation report
         report_file = os.path.join(dp2_job.dir_output, "html-report/report.xhtml")
         if os.path.isfile(report_file):
             with open(report_file, 'r') as result_report:
                 self.utils.report.attachment(result_report.readlines(), os.path.join(self.utils.report.reportDir(), "report.html"), "SUCCESS" if dp2_job.status == "DONE" else "ERROR")
-        
+
         if dp2_job.status != "DONE":
             self.utils.report.error("Klarte ikke å validere boken")
             self.utils.report.title = self.title + ": " + epub.identifier() + " feilet 😭👎"
             return
-        
+
         try:
             self.utils.report.info("Genererer ACE-rapport...")
             ace_dir = os.path.join(self.utils.report.reportDir(), "accessibility-report")
             process = self.utils.filesystem.run([IncomingNordic.ace_cli, "-o", ace_dir, epub.asFile()])
-            
+
             # attach report
             ace_status = None
             with open(os.path.join(ace_dir, "report.json")) as json_report:
@@ -87,23 +87,23 @@ class IncomingNordic(Pipeline):
             else:
                 ace_status = "WARN"
             self.utils.report.attachment(None, os.path.join(ace_dir, "report.html"), ace_status)
-            
+
         except subprocess.TimeoutExpired as e:
             self.utils.report.warn("Det tok for lang tid å lage ACE-rapporten for " + epub.identifier() + ", og prosessen ble derfor stoppet.")
-        
+
         except Exception:
             logging.exception("En feil oppstod ved produksjon av ACE-rapporten for " + epub.identifier())
             self.utils.report.warn("En feil oppstod ved produksjon av ACE-rapporten for " + epub.identifier())
-        
+
         self.utils.report.info("Boken er valid. Kopierer til EPUB master-arkiv.")
-        
+
         archived_path = self.utils.filesystem.storeBook(epub.asDir(), epub.identifier())
         UpdateMetadata.add_production_info(self, epub.identifier())
         self.utils.report.attachment(None, archived_path, "DEBUG")
         self.utils.report.success(epub.identifier()+" ble lagt til i master-arkivet.")
         self.utils.report.title = self.title + ": " + epub.identifier() + " er valid 👍😄"
         self.utils.filesystem.deleteSource()
-        
+
         # TODO:
         # - self.utils.epubCheck på mottatt EPUB
         # - EPUB 3 Accessibility Checker på mottatt EPUB
