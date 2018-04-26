6a426f5243d3403a5e02eeb023bac7b47a587fbf
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2018-04-26 10:15:31 +0200

    ignore 'Dolphin check log.txt'

--- produksjonssystem/core/utils/filesystem.py
+++ produksjonssystem/core/utils/filesystem.py
@@ -44,7 +44,7 @@ class Filesystem():
         "$RECYCLE.BIN/", "*~", ".fuse_hidden*", ".directory", ".Trash-*", ".nfs*", ".DS_Store", ".AppleDouble",
         ".LSOverride", "._*", ".DocumentRevisions-V100", ".fseventsd", ".Spotlight-V100", ".TemporaryItems",
         ".Trashes", ".VolumeIcon.icns", ".com.apple.timemachine.donotpresent", ".AppleDB", ".AppleDesktop",
-        "Network Trash Folder", "Temporary Items", ".apdisk"
+        "Network Trash Folder", "Temporary Items", ".apdisk", "Dolphin check log.txt"
     )
     
     def __init__(self, pipeline):
