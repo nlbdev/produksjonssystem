980af0d39e88f05ba42e52d67cc2960725c2aa18
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2018-05-02 16:37:37 +0200

    xslt.py: allow running XSLTs from without an associated pipeline

--- produksjonssystem/core/utils/xslt.py
+++ produksjonssystem/core/utils/xslt.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import os
-import re
-import time
-import tempfile
+import traceback
 import subprocess
 
+from core.utils.filesystem import Filesystem
 from core.utils.daisy_pipeline import DaisyPipelineJob
+
 
 class Xslt():
     """Class used to run XSLTs"""
-    
+
     # treat as class variables
     xslt_dir = os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../..", "xslt"))
     saxon_jar = None
@@ -18,26 +18,28 @@ class Xslt():
         "The XSLT": "XSLTen",
         "took too long time and was therefore stopped.": "tok for lang tid og ble derfor stoppet.",
     }
-    
-    # treat as instance variables
-    pipeline = None
-    
+
     @staticmethod
     def init_environment():
         DaisyPipelineJob.init_environment()
         Xslt.saxon_jar = os.path.join(DaisyPipelineJob.dp2_home, "system/framework/org.daisy.libs.saxon-he-9.5.1.5.jar")
-    
-    def __init__(self, pipeline=None, stylesheet=None, source=None, target=None, parameters={}, template=None, stdout_level="INFO", stderr_level="INFO"):
-        assert pipeline
+
+    def __init__(self, pipeline=None, stylesheet=None, source=None, target=None, parameters={}, template=None, stdout_level="INFO", stderr_level="INFO", report=None, cwd=None):
+        assert pipeline or cwd and report
         assert stylesheet
         assert source or template
         assert target
-        
-        self.pipeline = pipeline
+
+        if not report:
+            report = pipeline.utils.report
+
+        if not cwd:
+            cwd = report.pipeline.dir_in
+
         self.success = False
-        
+
         Xslt.init_environment()
-        
+
         try:
             command = ["java", "-jar", Xslt.saxon_jar]
             if source:
@@ -48,19 +50,19 @@ class Xslt():
             command.append("-o:" + target)
             for param in parameters:
                 command.append(param + "=" + parameters[param])
-            
-            self.pipeline.utils.report.debug("Running XSLT")
-            process = self.pipeline.utils.filesystem.run(command, stdout_level=stdout_level, stderr_level=stderr_level)
+
+            report.debug("Running XSLT")
+            Filesystem.run_static(command, cwd, report, stdout_level=stdout_level, stderr_level=stderr_level)
             self.success = True
-            
-        except subprocess.TimeoutExpired as e:
-            self.pipeline.utils.report.error(Xslt._i18n["The XSLT"] + " " + stylesheet + " " + Xslt._i18n["took too long time and was therefore stopped."])
-            
+
+        except subprocess.TimeoutExpired:
+            report.error(Xslt._i18n["The XSLT"] + " " + stylesheet + " " + Xslt._i18n["took too long time and was therefore stopped."])
+
         except Exception:
-            self.pipeline.utils.report.error("An error occured while running the XSLT (" + str(stylesheet) + ")")
-    
+            report.debug(traceback.format_exc())
+            report.error("An error occured while running the XSLT (" + str(stylesheet) + ")")
+
     # in case you want to override something
     @staticmethod
     def translate(english_text, translated_text):
         Filesystem._i18n[english_text] = translated_text
-    
