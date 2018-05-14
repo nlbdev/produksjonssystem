a95ea01243588e517f65e2aa610767c3110b46f5
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2018-05-14 15:13:11 +0200

    testProdsys.py: check all output dirs

--- tests/testProdsys.py
+++ tests/testProdsys.py
@@ -6,6 +6,7 @@
 #
 
 import os
+import re
 from shutil import copyfile
 from shutil import rmtree
 import sys
@@ -18,6 +19,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 prodsys_path = os.path.join(project_root, "produksjonssystem")
 sys.path.insert(0, prodsys_path)
 from produksjonssystem import run
+from core.pipeline import DummyPipeline
 
 # make target directory
 target_path = os.path.join(project_root, "target")
@@ -75,18 +77,6 @@ for d in book_archive_dirs:
     print("Book archive \"{}\": file://{}".format(d, book_archive_dirs[d]))
 print("")
 
-BookID = "558237"
-newBookID = "356837"
-epubInnL_ID = "406837"
-
-distPath = os.path.join(book_archive_dirs["master"], 'distribusjonsformater')
-DTBook_path = os.path.join(distPath, 'DTBook', BookID)
-epubInnL_path = os.path.join(distPath, 'EPUB-til-innlesing', epubInnL_ID+'.epub')
-DTBookToTts_path = os.path.join(distPath, 'DTBook-til-talesyntese', BookID)
-HTML_path = os.path.join(distPath, 'HTML', newBookID)
-PEF_path = os.path.join(distPath, 'PEF/fullskrift', BookID)
-DOCX_path = os.path.join(distPath, 'DOCX', newBookID)
-
 threading.current_thread().setName("test thread")
 print("Initializing the system...")
 prodsys = run.Produksjonssystem(environment=environment)
@@ -100,57 +90,74 @@ else:
     print("Timed out when starting system")
     sys.exit(1)
 
-file_path = os.path.join(os.path.dirname(__file__), BookID + ".epub")
-copyfile(file_path, os.path.join(book_archive_dirs["master"], 'innkommende', BookID + ".epub"))
+identifiers = ["558237", "115437", "221437", "356837", "406837"]
+file_path = os.path.join(os.path.dirname(__file__), identifiers[0] + ".epub")
+copyfile(file_path, os.path.join(prodsys.dirs["incoming"], os.path.basename(file_path)))
 
 for pipeline in prodsys.pipelines:
     if pipeline[0].uid == "create-abstracts":
         print("abstracts pipeline are not connected to the rest of the system. see: https://github.com/nlbdev/produksjonssystem/issues/99")
         pipeline[0].stop(exit=True)
 
+expect_dirs = {}
+for pipeline in prodsys.pipelines:
+    if (not pipeline[0].uid in ["update-metadata", "create-abstracts"]
+       and not isinstance(pipeline[0], DummyPipeline)
+       and pipeline[2]):
+        expect_dirs[pipeline[0].uid] = {
+            "title": pipeline[0].title,
+            "dir": pipeline[0].dir_out,
+            "status": None
+        }
+
 success = 1
 t = 500
 
-print("Starting test of NLB production system. Verifies distribution formats for " + BookID + ".epub in {0} seconds \n".format(t))
+print("Starting test of NLB production system. Verifies distribution formats for " + os.path.basename(file_path) + " in {0} seconds \n".format(t))
+
+
+def check_dirs(last_run=False):
+    global expect_dirs
+    global identifiers
+
+    success = True
+    for uid in expect_dirs:
+        expect_dir = expect_dirs[uid]["dir"]
+        if expect_dirs[uid]["status"] is not None:
+            success = success and expect_dirs[uid]["status"]
+            continue
+        for identifier in identifiers:
+            if os.path.isdir(os.path.join(expect_dir, identifier)):
+                expect_dirs[uid]["status"] = True
+
+            file = os.listdir(expect_dir)
+            file = re.sub(r'\.[^.]*$', '', file[0]) if file else None
+            if file and file == identifier:
+                expect_dirs[uid]["status"] = True
+
+            if expect_dirs[uid]["status"] is True:
+                result(expect_dirs[uid]["title"], True)
+                break
+
+            elif last_run:
+                result(expect_dirs[uid]["title"], False)
+                success = False
+                break
+
 
 start_time = int(time.time())
-prodsys_thread.join(timeout=t)
+while prodsys_thread.is_alive() and t > 0:
+    t -= 1
+    prodsys_thread.join(timeout=1)
+    check_dirs()
 end_time = int(time.time())
+
+success = check_dirs(last_run=True)
 
 if prodsys_thread.is_alive():
     print("The tests timed out after {} seconds".format(t))
 else:
     print("The tests finished after {} seconds".format(end_time - start_time))
-
-if os.path.exists(DTBookToTts_path):
-    result("DTBook til talesyntese  is verified", True)
-else:
-    result("DTBook til talesyntese does not exist", False)
-    success = 0
-
-if os.path.exists(epubInnL_path):
-    result("Epub til innlesing  is verified", True)
-else:
-    result("Epub til innlesing does not exist", False)
-    success = 0
-
-if os.path.exists(HTML_path):
-    result("HTML  is verified", True)
-else:
-    result("HTML does not exist", False)
-    success = 0
-
-if os.path.exists(DOCX_path):
-    result("DOCX  is verified", True)
-else:
-    result("DOCX does not exist", False)
-    success = 0
-
-if os.path.exists(PEF_path):
-    result("PEF fullskrift  is verified", True)
-else:
-    result("PEF fullskrift does not exist", False)
-    success = 0
 
 if (success):
     print("Tests succeeded")
