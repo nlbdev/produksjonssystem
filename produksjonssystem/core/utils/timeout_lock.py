520149a037151f31086f22c2cfe2b90aad1ae65f
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2018-06-05 17:31:27 +0200

    added utility class: TimeoutLock

---
+++ produksjonssystem/core/utils/timeout_lock.py
@@ -0,0 +1,21 @@
+import threading
+from contextlib import contextmanager
+
+
+# https://stackoverflow.com/a/16782490/281065
+class TimeoutLock(object):
+    def __init__(self):
+        self._lock = threading.Lock()
+
+    def acquire(self, blocking=True, timeout=-1):
+        return self._lock.acquire(blocking, timeout)
+
+    @contextmanager
+    def acquire_timeout(self, timeout):
+        result = self._lock.acquire(timeout=timeout)
+        yield result
+        if result:
+            self._lock.release()
+
+    def release(self):
+        self._lock.release()
