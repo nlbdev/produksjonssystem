ccadf3c1868d083c4a78b4e3eb559c1e29dc6660
Espen Solhjem <espen.solhjem@nlb.no>
2018-07-04 14:40:06 +0200

    nlbpub_previous v2

--- produksjonssystem/nlbpub_previous.py
+++ produksjonssystem/nlbpub_previous.py
@@ -7,8 +7,9 @@ import shutil
 import sys
 import tempfile
 import time
-
-from core.pipeline import Pipeline
+import filecmp
+import yaml
+from os import walk
 from core.utils.epub import Epub
 
 if sys.version_info[0] != 3 or sys.version_info[1] < 5:
@@ -61,64 +62,90 @@ class NlbpubPrevious(Pipeline):
         temp_epubdir = temp_epubdir_obj.name
         self.utils.filesystem.copy(self.book["source"], temp_epubdir)
 
+        time_created = time.strftime("%Y-%m-%dT%H:%M:%S")
+
         if not os.path.exists(os.path.join(self.dir_out, epub.identifier())):
             os.makedirs(os.path.join(self.dir_out, epub.identifier()))
 
-        dirlist = os.listdir(os.path.join(self.dir_out, epub.identifier()))
+        dictfiles = {}
+        changelog = "changelog.txt"
+        deleted = "deleted.yml"
+        changes_made = False
 
-        identical_dir_exists = False
+        # Dictfiles contains the most recent version of each file, can save this in a yaml file, like newest_files.yml
+        for (path, subdir_list, file_list) in walk(os.path.join(self.dir_out, epub.identifier())):
+            for file_name in file_list:
+                if file_name == changelog:
+                    break
+                file_path = os.path.join(self.dir_out, epub.identifier(), path, file_name)
+                new_dict = {file_name: file_path}
+                if file_name not in dictfiles:
+                    dictfiles.update(new_dict)
+                elif dictfiles[file_name] < file_path:
+                    dictfiles.update(new_dict)
 
-        # If an identical dir exists, delete contents of tempdir to prevent several of the same version being saved
-        for dir in dirlist:
-            if self.dirs_equal(os.path.join(self.dir_out, epub.identifier(), dir), temp_epubdir):
-                identical_dir_exists = True
-                for file in os.listdir(temp_epubdir):
-                    file_path = os.path.join(temp_epubdir, file)
-                    print(file_path)
-                    if os.path.isfile(file_path):
-                        os.remove(file_path)
-                    if os.path.isdir(file_path):
-                        shutil.rmtree(file_path)
-                break
-
-        time_created = time.strftime("%Y-%m-%dT%H:%M:%S")
-
-        # Save copy of new NLBPUB master. Different versions under NLBPUB-tidligere/xxxxxxx/time
-        if not identical_dir_exists:
-            self.utils.report.info("Ny backup ble laget. Kopierer til NLBPUB-previous-arkiv.")
+        changelog_path = os.path.join(self.dir_out, epub.identifier(), changelog)
+        if os.path.exists(changelog_path):
+            append_write = 'a'  # append if already exists
         else:
-            self.utils.report.info("Identisk versjon ble funnet. Lagrer en tom mappe")
+            append_write = 'w'  # make a new file if not
+        changelog_file = open(changelog_path, append_write)
+        new_file_list = []
+
+        # Changelog.txt contains the history of changes to this nlbpub with timestamps
+        for temp_path, temp_subdir_list, temp_file_list in walk(temp_epubdir):
+            for temp_file in temp_file_list:
+                new_file_list.append(temp_file)
+                temp_file_path = os.path.join(temp_epubdir, temp_path, temp_file)
+
+                if temp_file in dictfiles and filecmp.cmp(temp_file_path, dictfiles[temp_file]):
+                    os.remove(temp_file_path)
+                    # Remove empty folders
+                    if len(os.listdir(temp_path)) == 0:
+                        os.rmdir(temp_path)
+
+                elif temp_file in dictfiles and not filecmp.cmp(temp_file_path, dictfiles[temp_file]):
+                    changes_made = True
+                    self.utils.report.info("Fil endret: " + temp_file)
+                    changelog_file.write("\n{}:     Fil endret: {}".format(time_created, temp_file))
+        folders = list(os.walk(temp_epubdir))[1:]
+
+        # Remove the rest of the empty dirs
+        for folder in folders:
+            if not folder[2]:
+                os.rmdir(folder[0])
+
+        # Overview of deleted files, yaml file with delete history is located at deleted_path
+        deleted_path = os.path.join(self.dir_out, epub.identifier(), deleted)
+        if os.path.exists(deleted_path):
+            deleted_append_write = 'a'  # append if already exists
+        else:
+            deleted_append_write = 'w'  # make a new file if not
+        deleted_file = open(deleted_path, deleted_append_write)
+
+        with open(deleted_path, 'r') as f:
+            deleted_doc = yaml.load(f) or {}
+
+        # Deleted file history
+        for key in dictfiles:
+                if key not in new_file_list and key not in deleted_doc:
+                    changes_made = True
+                    self.utils.report.info("Fil slettet: " + key)
+                    changelog_file.write("\n{}:     Fil slettet: {}".format(time_created, key))
+                    deleted_file.write("\n{}: {}".format(key, dictfiles[key]))
+        changelog_file.close()
+
+        # Save copy of different files in NLBPUB master. Different versions of files under NLBPUB-tidligere/xxxxxxx/time
+        # To restore a certain version copy files from the each folder up to the wanted version to a new folder
 
         archived_path = self.utils.filesystem.storeBook(temp_epubdir, epub.identifier(), subdir=time_created)
         self.utils.report.attachment(None, archived_path, "DEBUG")
-        self.utils.report.info(epub.identifier() + " ble lagt til i NLBPUB-previous-arkivet.")
-        self.utils.report.title = self.title + ": " + epub.identifier() + " ble kopiert 👍😄" + epubTitle
-        return True
-
-    def dirs_equal(self, dir1, dir2):
-        """
-        Compare two directories recursively. Files in each directory are
-        assumed to be equal if their names and contents are equal.
-
-        @param dir1: First directory path
-        @param dir2: Second directory path
-
-        @return: True if the directory trees are the same and
-            there were no errors while accessing the directories or files,
-            False otherwise.
-       """
-
-        dirs_cmp = filecmp.dircmp(dir1, dir2)
-        if len(dirs_cmp.left_only) > 0 or len(dirs_cmp.right_only) > 0 or len(dirs_cmp.funny_files) > 0:
-            return False
-        (_, mismatch, errors) = filecmp.cmpfiles(dir1, dir2, dirs_cmp.common_files, shallow=False)
-        if len(mismatch) > 0 or len(errors) > 0:
-            return False
-        for common_dir in dirs_cmp.common_dirs:
-            new_dir1 = os.path.join(dir1, common_dir)
-            new_dir2 = os.path.join(dir2, common_dir)
-            if not self.dirs_equal(new_dir1, new_dir2):
-                return False
+        if changes_made:
+            self.utils.report.info("Endringer oppdaget for: " + epub.identifier() + ", endrede filer ble kopiert til NLBpub tidligere versjoner.")
+            self.utils.report.title = self.title + ": " + epub.identifier() + " 👍😄" + epubTitle
+        else:
+            self.utils.report.info("Ingen endringer oppdaget for " + epub.identifier())
+            self.utils.report.should_email = False
         return True
 
 
