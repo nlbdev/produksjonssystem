62fe889316192c30122f42477974d7eb9d1c3bed
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2019-10-21 10:29:10 +0000

    don't fail when slack is not configured

--- produksjonssystem/core/utils/slack.py
+++ produksjonssystem/core/utils/slack.py
@@ -7,9 +7,9 @@ import slack
 class Slack():
 
     _slack = None
-    _slack_token = os.getenv("SLACK_TOKEN", None)
-    _slack_authed = None
-    _slack_channel = os.getenv("SLACK_CHANNEL", "#test")
+    slack_token = os.getenv("SLACK_BOT_TOKEN", None)
+    slack_authed = None
+    slack_channel = os.getenv("SLACK_CHANNEL", "#test")
 
     @staticmethod
     def slack(text, attachments, retry=True):
@@ -18,27 +18,27 @@ class Slack():
         assert text or attachments
 
         # If not authenticated: try to authenticate and test authorization
-        if not Slack._slack_authed or Slack._slack is None:
+        if (not Slack.slack_authed or Slack._slack is None) and Slack.slack_token is not None:
             logging.info("Connecting to Slack…")
-            Slack._slack = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+            Slack._slack = slack.WebClient(token=Slack.slack_token)
             try:
                 logging.info("Verifying connection to Slack…")
                 auth = Slack._slack.auth_test()
                 if auth.data.get("ok"):
                     logging.info("Slack authenticated as \"{}\" as part of the team \"{}\"".format(auth.data.get("user"), auth.data.get("team")))
-                    Slack._slack_authed = True
+                    Slack.slack_authed = True
                 else:
                     logging.warning("Failed to authenticate to Slack")
-                    Slack._slack_authed = False
+                    Slack.slack_authed = False
 
             except Exception:
                 logging.exception("Failed to authenticate to Slack")
-                Slack._slack_authed = False
+                Slack.slack_authed = False
 
         # Try to send
-        if Slack._slack_authed:
+        if Slack.slack_authed:
             try:
-                Slack._slack.chat_postMessage(channel=Slack._slack_channel, text=text)  # TODO: fix attachments with slack library
+                Slack._slack.chat_postMessage(channel=Slack.slack_channel, text=text)  # TODO: fix attachments with slack library
             except Exception:
                 Slack._slack = None  # set to None so that we try to create a new connection when sending the next message
                 if retry:
@@ -48,4 +48,4 @@ class Slack():
 
         else:
             logging.warning("Not authenticated to send messages to Slack")
-            logging.warning("Tried to send message to {}: {}".format(Slack._slack_channel, text))
+            logging.warning("Tried to send message to {}: {}".format(Slack.slack_channel, text))
