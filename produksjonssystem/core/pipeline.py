nlbdev/produksjonssystem

commit 4ed69329b0430d5e3596643629adb9396eeaf02d

retrying books: get the identifiers from the input EPUB if possible

produksjonssystem/core/pipeline.py
@@ -20,6 +20,7 @@ from dotmap import DotMap
 
 from core.config import Config
 from core.directory import Directory
+from core.utils.epub import Epub
 from core.utils.filesystem import Filesystem
 from core.utils.metadata import Metadata
 from core.utils.report import DummyReport, Report
@@ -731,12 +732,27 @@ class Pipeline():
 
                 # If input file is an epub (starts with 5), find all possible identifiers
                 if book_path is None and self.check_identifiers:
+                    # try getting identifiers from package.opf (fast)
                     try:
                         self.pipelineDummy = DummyPipeline(uid=self.uid + "-auto", title=self.title + fileStem + " retry")
-                        issue, edition = Metadata.get_identifiers(self.pipelineDummy.utils.report, fileStem)
+
+                        epub = Epub(self.pipelineDummy, book_path)
+                        if epub.isepub():
+                            book_metadata = epub.metadata()
+                            for property in book_metadata:
+                                if (property == "dc:identifier" or property.startswith("nlbprod:identifier.")) and book_metadata[property] not in issue:
+                                    issue.append(book_metadata[property])
+
                     except Exception:
                         logging.info("Metadata feilet under get_identifiers for fileStem")
-                    # TODO Maybe if not epub and not daisy202 find epub identifier from metadata then call to Metadata to find editions
+
+                    if len(issue) == 1:
+                        # if we didn't find any new identifiers in the EPUB: try getting identifiers from Quickbase and Bibliofil (slow)
+                        try:
+                            self.pipelineDummy = DummyPipeline(uid=self.uid + "-auto", title=self.title + fileStem + " retry")
+                            issue, edition = Metadata.get_identifiers(self.pipelineDummy.utils.report, fileStem)
+                        except Exception:
+                            logging.info("Metadata feilet under get_identifiers for fileStem")
 
                     try:
                         book_path = Filesystem.book_path_in_dir(self.dir_out, issue, subdirs=self.parentdirs)

commit c7e9d63ff10821f77d0d18fedfe01bed95449b2c

fix error introduced in 4ed69329

produksjonssystem/core/pipeline.py
@@ -736,7 +736,7 @@ class Pipeline():
                     try:
                         self.pipelineDummy = DummyPipeline(uid=self.uid + "-auto", title=self.title + fileStem + " retry")
 
-                        epub = Epub(self.pipelineDummy, book_path)
+                        epub = Epub(self.pipelineDummy, path)
                         if epub.isepub():
                             book_metadata = epub.metadata()
                             for property in book_metadata:
@@ -744,7 +744,7 @@ class Pipeline():
                                     issue.append(book_metadata[property])
 
                     except Exception:
-                        logging.info("Metadata feilet under get_identifiers for fileStem")
+                        logging.exception("Metadata feilet under Epub.metadata() for {}".format(fileStem))
 
                     if len(issue) == 1:
                         # if we didn't find any new identifiers in the EPUB: try getting identifiers from Quickbase and Bibliofil (slow)
@@ -752,7 +752,7 @@ class Pipeline():
                             self.pipelineDummy = DummyPipeline(uid=self.uid + "-auto", title=self.title + fileStem + " retry")
                             issue, edition = Metadata.get_identifiers(self.pipelineDummy.utils.report, fileStem)
                         except Exception:
-                            logging.info("Metadata feilet under get_identifiers for fileStem")
+                            logging.info("Metadata feilet under get_identifiers for {}".format(fileStem))
 
                     try:
                         book_path = Filesystem.book_path_in_dir(self.dir_out, issue, subdirs=self.parentdirs)