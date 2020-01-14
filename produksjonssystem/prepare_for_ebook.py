22430197a1fd6e85e1188f49dd8e34440e4c2252
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2020-01-14 15:27:57 +0100

    prepare-for-ebook: rename to 'Klargjør for e-bok'

--- produksjonssystem/prepare_for_ebook.py
+++ produksjonssystem/prepare_for_ebook.py
@@ -22,7 +22,7 @@ if sys.version_info[0] != 3 or sys.version_info[1] < 5:
 
 class PrepareForEbook(Pipeline):
     uid = "prepare-for-ebook"
-    title = "NLBPUB til e-bok"
+    title = "Klargjør for e-bok"
     labels = ["e-bok", "Statped"]
     publication_format = "XHTML"
     expected_processing_time = 8
