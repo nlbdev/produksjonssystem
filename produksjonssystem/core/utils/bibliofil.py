bc2284ece97a88c07a9be41e568d8341c489d623
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2020-07-01 11:42:36 +0200

    formatklar: removed DOCX and renamed from 'Kindle' to 'lesebrett'

--- produksjonssystem/core/utils/bibliofil.py
+++ produksjonssystem/core/utils/bibliofil.py
@@ -35,12 +35,9 @@ class Bibliofil:
             if has_html:
                 lines.append("{};{};{};{}".format(identifier, "html", "dl", "HTML"))
 
-            if has_docx and library == "Statped":
-                lines.append("{};{};{};{}".format(identifier, "docx", "dl", "DOCX (Word)"))
-
             if has_mobi:
                 lines.append("{};{};{};{}".format(identifier, "mobi", "dl", "Mobi / Kindle Format"))
-                lines.append("{};{};{};{}".format(identifier, "mobi", "ki", "Send til Kindle"))
+                lines.append("{};{};{};{}".format(identifier, "mobi", "ki", "Send til lesebrett"))
 
             if has_epub:
                 lines.append("{};{};{};{}".format(identifier, "epub", "no", "Legg til på bokhylla"))
