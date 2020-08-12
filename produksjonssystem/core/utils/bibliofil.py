nlbdev/produksjonssystem

commit 994214ad95b19a18a3194bc026fec460f21588a1

when sending formatklar, also send filesize

produksjonssystem/core/utils/bibliofil.py
@@ -24,10 +24,28 @@ class Bibliofil:
         if format == "XHTML":  # "XHTML" means e-book (we're reusing identifiers previously reserved for XHTML files)
             lines = []
 
-            has_epub = os.path.isdir(os.path.join(Directory.dirs_flat["epub-ebook"], identifier))
+            epub_dir = os.path.join(Directory.dirs_flat["epub-ebook"], identifier)
+            has_epub = os.path.isdir(epub_dir)
             has_html = has_epub  # generated based on the EPUB on the fly
             has_docx = has_epub  # generated based on the EPUB on the fly
             has_mobi = has_epub  # generated based on the EPUB on the fly
+
+            size = 0
+            if has_epub:
+                for root, dirs, files in os.walk(epub_dir):
+                    for file in files:
+                        size += os.path.getsize(file)
+
+                filesize_xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
+                filesize_xml += "<root>\n"
+                filesize_xml += "<folder><name>{}</name><sizedata>{}</sizedata></folder>\n".format(identifier, size)
+                filesize_xml += "</root>\n"
+
+                logging.info("Sending filesize-e-mail to {} with content:".format(Config.get("email.filesize.address")))
+                logging.info(filesize_xml)
+                Report.emailPlainText("filesize: " + identifier,
+                                      filesize_xml,
+                                      Config.get("email.filesize.address"))
 
             if has_epub:
                 lines.append("{};{};{};{}".format(identifier, "epub", "dl", "EPUB"))

produksjonssystem/run.py
@@ -123,6 +123,7 @@ class Produksjonssystem():
         Config.set("email.smtp.user", os.environ.get("MAIL_USERNAME", None))
         Config.set("email.smtp.pass", os.environ.get("MAIL_PASSWORD", None))
         Config.set("email.formatklar.address", os.environ.get("MAIL_FORMATKLAR"))
+        Config.set("email.formatklar.address", os.environ.get("MAIL_FILESIZE"))
 
         # Configure NLB API URL
         Config.set("nlb_api_url", os.environ.get("NLB_API_URL"))

set-test-env.sh.example
@@ -13,6 +13,7 @@ export MAIL_USERNAME=""
 export MAIL_PASSWORD=""
 
 export MAIL_FORMATKLAR=
+export MAIL_FILESIZE=
 export ALLOWED_EMAIL_ADDRESSES_IN_TEST=  # comma separated list of e-mails
 
 export BOOK_ARCHIVE_DIRS="master=/tmp/book-archive share=/tmp/book-archive-share distribution=/tmp/book-archive-distribution news=/tmp/book-archive-news"

commit 83807eda4c5d3ee9072f159fbe1a9ddc59e71036

use full path for calculating filesize (issue introduced in 994214ad)

produksjonssystem/core/utils/bibliofil.py
@@ -34,7 +34,7 @@ class Bibliofil:
             if has_epub:
                 for root, dirs, files in os.walk(epub_dir):
                     for file in files:
-                        size += os.path.getsize(file)
+                        size += os.path.getsize(os.path.join(root, file))
 
                 filesize_xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
                 filesize_xml += "<root>\n"