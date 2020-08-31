2a36772cd86fe3b168dabaf8ab33dae0eb006359
Espen Solhjem <espen.solhjem@nlb.no>
2020-08-31 13:55:13 +0200

    attempt to increase timeout

--- produksjonssystem/core/pipeline.py
+++ produksjonssystem/core/pipeline.py
@@ -417,7 +417,7 @@ class Pipeline():
                         # found thread that is not running
                         logging.debug("not alive: {}".format(thread))
                         return False
-                    if thread in self.watchdogs and time.time() - self.watchdogs[thread] > 3600:
+                    if thread in self.watchdogs and time.time() - self.watchdogs[thread] > 10800:
                         # found thread that is frozen
                         logging.debug("frozen: {}".format(thread))
                         return False
