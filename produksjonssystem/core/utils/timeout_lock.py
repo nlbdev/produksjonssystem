2acbddb3bbc9d078eb41bfe98ab41161662814d3
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2020-09-17 14:26:39 +0200

    timeout_lock.py: use a reentrant lock (RLock)
    This allows a thread to lock the lock multiple times.
    Useful for instance for recursive functions.
    
    Should fix issue with choose_engine().

--- produksjonssystem/core/utils/timeout_lock.py
+++ produksjonssystem/core/utils/timeout_lock.py
@@ -5,7 +5,7 @@ from contextlib import contextmanager
 # https://stackoverflow.com/a/16782490/281065
 class TimeoutLock(object):
     def __init__(self):
-        self._lock = threading.Lock()
+        self._lock = threading.RLock()
 
     def acquire(self, blocking=True, timeout=-1):
         return self._lock.acquire(blocking, timeout)
