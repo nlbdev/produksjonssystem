76619bef250d74336e8963c83056bb6b4f498d9a
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2022-04-04 23:51:17 +0200

    nlbpub-to-pef: allow Pipeline 2 v1.14.5 and v.1.14.6 for Statped

--- produksjonssystem/nlbpub_to_pef.py
+++ produksjonssystem/nlbpub_to_pef.py
@@ -215,6 +215,8 @@ class NlbpubToPef(Pipeline):
             # use DAISYs version of PIP instead
             script_id = "html-to-pef"
             pipeline_and_script_version = [
+                ("1.14.6", None),
+                ("1.14.5", None),
                 ("1.14.4", "4.2.0"),
                 ("1.14.4-SNAPSHOT", "4.1.1"),
                 ("1.14.3", "4.1.1"),
