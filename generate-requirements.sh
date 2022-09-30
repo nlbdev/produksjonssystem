cbbf19d02c9869854e717027c7e521ee744fb239
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2022-09-30 16:38:47 +0200

    update requirements.txt

--- generate-requirements.sh
+++ generate-requirements.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-pipreqs . --force --print | sort > requirements.txt
+echo "First, load the correct python environment:"
+echo "source prodsys-virtualenv/bin/activate"
+echo
+echo "If you haven't already, install pipreqs and pip-tools."
+echo "You'll have to reload your environment after installing."
+echo "pip install pipreqs pip-tools"
+echo "source prodsys-virtualenv/bin/activate"
+echo 
+echo "Then, modify requirements.in manually."
+echo "You can also recreate it automatically, but you should"
+echo "then manually inspect it afterwards:"
+echo "pipreqs produksjonssystem --force --print | sed 's/=.*//' | sort > requirements.in"
+echo
+echo "Finally, compile a new requirements.txt:"
+echo "pip-compile requirements.in > requirements.txt"
