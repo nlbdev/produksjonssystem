d1e1b5f6b7f55f8709f3cd261c1c4573e66e7c11
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2022-12-06 21:47:33 +0100

    run.sh: added instructions about installing virtualenv

--- run.sh
+++ run.sh
@@ -4,6 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
 # load python environment
+if [ "`which virtualenv`" = "" ]; then
+    echo "virtualenv not installed."
+    echo "Install with:"
+    echo "  pip3 install virtualenv"
+    exit 1
+fi
 if [ ! -d "prodsys-virtualenv" ]; then
     virtualenv prodsys-virtualenv
     pip install -r requirements.txt
