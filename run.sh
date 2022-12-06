9e730a7890cfeb5b8e42afa05ee70ffdb75048d2
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2022-12-06 22:32:06 +0100

    run.sh: fix initial virtualenv setup

--- run.sh
+++ run.sh
@@ -11,7 +11,14 @@ if [ "`which virtualenv`" = "" ]; then
     exit 1
 fi
 if [ ! -d "prodsys-virtualenv" ]; then
-    virtualenv prodsys-virtualenv
+    if [ "`which python3.7`" = "" ]; then
+	echo "Python 3.7 not installed. Please install Pyton 3.7 before continuing. Suggestion:"
+        echo "sudo apt install software-properties-common -y"
+        echo "sudo add-apt-repository ppa:deadsnakes/ppa -y"
+        echo "sudo apt update"
+        echo "sudo apt install python3.7 python3.7-distutils"
+    fi
+    virtualenv --python="`which python3.7`" prodsys-virtualenv
     pip install -r requirements.txt
 fi
 source prodsys-virtualenv/bin/activate
