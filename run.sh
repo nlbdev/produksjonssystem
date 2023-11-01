dc6527f13c7c6304bca7fda8f03541333bb5b17c
Jostein Austvik Jacobsen <josteinaj@gmail.com>
2023-11-01 13:52:16 +0100

    updated run.sh

--- run.sh
+++ run.sh
@@ -7,17 +7,20 @@ cd "$DIR"
 if [ "`which virtualenv`" = "" ]; then
     echo "virtualenv not installed."
     echo "Install with:"
-    echo "  pip3 install virtualenv"
+    #echo "  pip3 install virtualenv"
+    echo "  sudo apt install python3-virtualenv"
+    exit 1
+fi
+if [ "`which python3.7`" = "" ]; then
+    echo "Python 3.7 not installed. Please install Pyton 3.7 before continuing. Suggestion:"
+    echo "sudo apt install software-properties-common -y"
+    echo "echo \"deb [trusted=yes] https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu jammy main\" | sudo tee /etc/apt/sources.list.d/deadsnakes.list"
+    echo "echo \"deb-src [trusted=yes] https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu jammy main\" | sudo tee -a /etc/apt/sources.list.d/deadsnakes.list"
+    echo "sudo apt update"
+    echo "sudo apt install python3.7 python3.7-distutils"
     exit 1
 fi
 if [ ! -d "prodsys-virtualenv" ]; then
-    if [ "`which python3.7`" = "" ]; then
-	echo "Python 3.7 not installed. Please install Pyton 3.7 before continuing. Suggestion:"
-        echo "sudo apt install software-properties-common -y"
-        echo "sudo add-apt-repository ppa:deadsnakes/ppa -y"
-        echo "sudo apt update"
-        echo "sudo apt install python3.7 python3.7-distutils"
-    fi
     virtualenv --python="`which python3.7`" prodsys-virtualenv
     pip install -r requirements.txt
 fi
